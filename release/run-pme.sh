ca93683fba19162664ba256d3b1346ccd81ea572
Sergey Beryozkin <sberyozkin@gmail.com>
2018-09-13 12:54:45 +0100

    Adding Ladislav's run_pme.sh to the release folder

---
+++ release/run-pme.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+BASE_URL=http://repo1.maven.org/maven2/org/commonjava/maven/ext/pom-manipulation-cli
+VERSION=$(curl --silent $BASE_URL/maven-metadata.xml | xmlstarlet select --template --value-of '/metadata/versioning/latest')
+FILE=pom-manipulation-cli-$VERSION.jar
+if [[ ! -f $FILE ]] ; then
+  echo "Downloading $FILE"
+  curl --silent --remote-name $BASE_URL/$VERSION/$FILE
+  echo "Downloaded $FILE"
+else
+  echo "Using existing $FILE"
+fi
+
+java -jar $FILE -DdependencySource=REST -DversionIncrementalSuffix=redhat -DrestURL=http://da.cloud.pnc.engineering.redhat.com/da/rest/v-1
